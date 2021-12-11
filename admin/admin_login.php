3f9c1eac9deaaf1d769f5bce68a49c312f39605b
[tuongdangvuongquoc901@GMAIL.COM] <tuongdangvuongquoc901@gmail.com>
2021-12-11 23:46:54 +0700

    update admin

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -5,7 +5,7 @@
     $query = "SELECT * FROM login_use Where Use_Name = '$us' and Pass_Word = '$pw'";
     $data = mysqli_query($conn,$query);
     if( mysqli_num_rows($data)>0) {
-        header("location: /Web_Project_11/Web_Project_group_11/admin/index.php");
+        header("location: /Web_Project_11/Web_Project_group_11/admin/index.php");       
     }else{
         header("location: /Web_Project_11/Web_Project_group_11/admin/login.php");
     }
---
+++ admin/footer.php
@@ -0,0 +1,7 @@
+<footer class="page-footer font-small blue">
+
+<div class="footer-copyright text-center py-3">© 61THNB-nhóm 8:
+  <a href="#">hahalolo.com</a>
+</div>
+
+</footer>
