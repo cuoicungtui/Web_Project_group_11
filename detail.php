582f15012308f934af48c8817ec1ddec8217014b
ATus <you@example.com>
2021-12-17 23:13:31 +0700

    ...

---
+++ detail.php
@@ -0,0 +1,22 @@
+<?php include('header.php') ?>
+<div class="contener">
+<h1 class="text-muted">HÀ NỘI ĐIỀU CHỈNH QUY ĐỊNH KIỂM SOÁT DỊCH ĐỐI VỚI NGƯỜI VỀ TỪ ĐỊA PHƯƠNG KHÁC</h1>
+
+</div>
+<div class="container">
+     
+      <p class="ms-auto">19/11/2020</p>
+          
+        </div>
+    </div>
+
+    <div class="container">
+     
+     <h6>HÀ NỘI ĐIỀU CHỈNH QUY ĐỊNH KIỂM SOÁT DỊCH ĐỐI VỚI NGƯỜI VỀ TỪ ĐỊA PHƯƠNG KHÁC </h6>
+         
+       </div>
+       <div class="imgs">
+       <img src="img.img208" class="figure-img img-fluid rounded mx-auto" alt="...">
+</div>
+
+<?php include('footer.php') ?>
