8957aafa70563523c24864ed9ce227d3f9173ea8
[tuongdangvuongquoc901@GMAIL.COM] <tuongdangvuongquoc901@gmail.com>
2021-12-19 10:52:34 +0700

    update login

--- admin/login_admin.php
+++ admin/login_admin.php
@@ -10,14 +10,14 @@
         $data = mysqli_fetch_assoc($result);
         if(password_verify($pw,$data['PASSWORD'])){
             $_SESSION['USERNAME_admin'] = $us;
-            header("location: /Web_Project_11/Web_Project_group_11/admin/Home.php");  
+            header("location:Home.php");  
         }else{
             $error = "1";
-            header("location: /Web_Project_11/Web_Project_group_11/admin/index.php?err=$error");
+            header("location:index.php?err=$error");
         }
              
     }else{
         $error = "2";
-        header("location: /Web_Project_11/Web_Project_group_11/admin/index.php?err=$error");
+        header("location:index.php?err=$error");
     }
 ?>
