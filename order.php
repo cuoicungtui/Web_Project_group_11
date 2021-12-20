f778186de2d5a6452272566e4b25db82029bf0c6
ATus <you@example.com>
2021-12-20 23:52:54 +0700

    up

---
+++ order.php
@@ -0,0 +1,17 @@
+<?php include('header.php') ?>
+<div class="container " style ="margin-top: var(--header-mg);" >
+    <div class="row">
+        <div class="col-md-12">
+            <div class = "manager-header d-flex justify-content-between">
+                <div class ="heading_title">Quản lý đơn hàng</div>
+                <div class ="tour d-flex ">
+                <p><i class="fa fa-plane"></i> Vé máy bay</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include('footer.php') ?>
