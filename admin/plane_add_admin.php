6cc5348aa8090c668cc6984e60b7d48994d68290
ATus <you@example.com>
2021-12-24 10:40:27 +0700

    up

--- admin/plane_add_admin.php
+++ admin/plane_add_admin.php
@@ -17,7 +17,7 @@ include "header_admin.php";
 <div class="container mb-5">
     <h3 class="text-center mt-3">Add Plane</h3>
 
-    <Form action="plane_add_admin_DB.php" method="Post" enctype="multipart/form-data">
+    <Form action="plane_add_admin_DB.php" method="Post">
         <div class="mb-3">
             <label for="Code_Plane" class="form-label">Code_Plane</label>
             <input type="text" class="form-control" id="Code_Plane" name="Code_Plane" placeholder="Code_Plane" required>
@@ -38,13 +38,13 @@ include "header_admin.php";
             <label for="L2_Chair" class="form-label">L2_Chair</label>
             <input type="number" class="form-control" id="L2_Chair" name="L2_Chair" placeholder="L2_Chair" required>
         </div>
-        
+        <div class="mb-3 ">
+            <input class="btn btn-primary ms-auto" name="submit" type="submit" value="Submit">
+        </div>  
+
     </Form>
     
-    <div class="mb-3 ">
-            <input class="btn btn-primary ms-auto" name="submit" type="submit" value="Submit">
-    </div>  
-
+    
 </div>
 
 <?php
--- admin/plane_add_admin_DB.php
+++ admin/plane_add_admin_DB.php
@@ -6,13 +6,18 @@
 
 
 <?php
+    require "connect.php";
     $code_plane = $_POST['Code_Plane'];
     $name_plane = $_POST['Name_Plane'];
     $name_airline = $_POST['Name_Airline'];
     $l1_chair = $_POST['L1_Chair'];
-    $l2_chair - $_POST['L2_Chair'];
+    $l2_chair = $_POST['L2_Chair'];
+    $query = "INSERT INTO list_planes (Code_Plane,Name_PLane,Name_Airline,L1_Chair,L2_Chair) VALUES ('$code_plane', '$name_plane', '$name_airline', $l1_chair,$l2_chair)";
+    $result =  mysqli_query($conn,$query);
+    if($result){
+       header('location:plane_admin.php');
+    }
+    else
+        header("location:plane_add_admin.php");
     
-    
-
-
 ?>
