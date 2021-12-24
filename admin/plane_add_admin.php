c28ac6a3d06b89eb268a607b6b3bb07f5ec286cd
ATus <you@example.com>
2021-12-24 09:36:25 +0700

    up

---
+++ admin/plane_add_admin.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+if (!isset($_SESSION['USERNAME_admin'])) {
+    header("location:index.php");
+    return;
+}
+?>
+
+<?php
+include "header_admin.php";
+?>
+
+<?php
+include "header_admin.php";
+?>
+
+<div class="container mb-5">
+    <h3 class="text-center mt-3">Add Plane</h3>
+
+    <Form action="plane_add_admin_DB.php" method="Post" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="Code_Plane" class="form-label">Code_Plane</label>
+            <input type="text" class="form-control" id="Code_Plane" name="Code_Plane" placeholder="Code_Plane" required>
+        </div>
+        <div class="mb-3">
+            <label for="Name_Plane" class="form-label">Name_Plane</label>
+            <input type="text" class="form-control" id="Name_Plane" name="Name_Plane" placeholder="Name_Plane" required>
+        </div>
+        <div class="mb-3">
+            <label for="Name_Airline" class="form-label">Name_Airline</label>
+            <input type="text" class="form-control" id="Name_Airline" name="Name_Airline" placeholder="Name_Airline" required>
+        </div>
+        <div class="mb-3">
+            <label for="L1_Chair" class="form-label">L1_Chair</label>
+            <input type="number" class="form-control" id="L1_Chair" name="L1_Chair" placeholder="L1_Chair" required>
+        </div>
+        <div class="mb-3">
+            <label for="L2_Chair" class="form-label">L2_Chair</label>
+            <input type="number" class="form-control" id="L2_Chair" name="L2_Chair" placeholder="L2_Chair" required>
+        </div>
+        
+    </Form>
+    
+    <div class="mb-3 ">
+            <input class="btn btn-primary ms-auto" name="submit" type="submit" value="Submit">
+    </div>  
+
+</div>
+
+<?php
+include "footer.php";
+?>
---
+++ admin/plane_add_admin_DB.php
@@ -0,0 +1,18 @@
+<?php
+    if(!isset($_POST['submit'])){
+        header('location:index.php');
+    }
+?>
+
+
+<?php
+    $code_plane = $_POST['Code_Plane'];
+    $name_plane = $_POST['Name_Plane'];
+    $name_airline = $_POST['Name_Airline'];
+    $l1_chair = $_POST['L1_Chair'];
+    $l2_chair - $_POST['L2_Chair'];
+    
+    
+
+
+?>
