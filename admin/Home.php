5d3666c90c1c38d91333979f5e99a48036a22fcc
[tuongdangvuongquoc901@GMAIL.COM] <tuongdangvuongquoc901@gmail.com>
2022-01-14 23:34:51 +0700

    End

--- admin/Home.php
+++ admin/Home.php
@@ -12,6 +12,27 @@ if (!isset($_SESSION['USERNAME_admin'])) {
 <div class="container  " style="margin-top: 5%;">
     <h3>Thông Tin </h3>
 </div>
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../assets/img/carosel1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/img/carosel2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/img/carosel3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
 <?php
  include "footer.php";
